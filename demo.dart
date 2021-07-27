0d6a9cc18213bee0c479183ff3741e998ffb9848
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-07-27 20:08:11 +0530

    string to int conversion

--- demo.dart
+++ demo.dart
@@ -1,15 +1,4 @@
-import 'TesterLibs.dart' as me;
-import 'dart:math'as m;
-main()
-{
-  print('hello world: ${me.calculate()}!');
-  print("hello ${me.getName()}");
-
-  var a=56;
-  var b=10;
-  print(me.addNumbers(a,b));
-  print(m.sqrt(10));
-  print("Max is ${m.max(10,123)}");
-
- print( new me.Person().name);//calling instance variable of class using constructor
+main () {
+  var onePointOne = double.parse('1');
+  print(onePointOne == 1); // prints true
 }
